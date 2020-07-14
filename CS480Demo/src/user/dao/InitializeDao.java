71c45a3623ff4b5889914fd9cb3f1befd3ce2f9a
Jonathan Vega <jon.e.vega@icloud.com>
2020-07-13 20:15:52 -0500

    project code outline complete! implemented init button for database

--- CS480Demo/src/user/dao/InitializeDao.java
+++ CS480Demo/src/user/dao/InitializeDao.java
@@ -26,9 +26,14 @@ public class InitializeDao {
 			
 			statement = connect.createStatement();
 			statement.executeUpdate("drop table if exists tb_user");
+			statement.executeUpdate("drop table if exists tb_company");
+			statement.executeUpdate("drop table if exists tb_job");
+
+			
+			/* create tb_user table */
 			
 			String sqlStatement1 = "create table if not exists tb_user (" +
-					"user_name VARCHAR(16) primary key," + 
+					"username VARCHAR(16) primary key," + 
 					"`password` VARCHAR(32) not null," + 
 					"first_name varchar(32) not null," + 
 					"last_name varchar(32) not null," + 
@@ -37,9 +42,48 @@ public class InitializeDao {
 					"language_proficiency varchar(16)" + 
 					");"
 					;
+			String sqlAdd1 = "insert into tb_user(username, password, first_name, last_name, is_admin, need_visa_sponsor, language_proficiency) values ('mitsuo8', 'blue', 'Amber', 'Sanfilip', 0, 0, 'JLPT 4'), ('pokemon_master', 'mew', 'Brock', 'Onix', 0, 1, 'Newb'), ('Excel', 'Tomoko', 'Jon', 'Vega', 1, 0, 'JLPT N5');";
 			
 			statement.executeUpdate(sqlStatement1);
 			
+			/* create tb_company table */
+			
+			String sqlStatement2 = "create table if not exists tb_company(" + 
+					"username varchar(16) primary key," + 
+					"password varchar(32) not null," + 
+					"company_name varchar(64) not null," + 
+					"company_location varchar(64) not null," + 
+					"company_bio text" + 
+					");"
+					;
+			
+			String sqlAdd2 = "insert into tb_company(username, password, company_name, company_location, company_bio) values ('rareware', 'banjo', 'Rare LTD.', 'Tokyo, Japan', 'We take too long in creating games from Xbox, and need people to get us going again!'), ('lawson', 'food', 'Lawson', 'Koriyama, Japan', 'We never close...'), ('loteria', 'melonsoda', 'Loteria', 'Iwaki, Japan', 'Insert something about chicken here');";
+			
+			statement.executeUpdate(sqlStatement2);
+			
+			/* create tb_job table */
+			
+			String sqlStatement3 = "create table if not exists tb_job(" + 
+					"job_id int auto_increment primary key," + 
+					"job_title varchar(64) not null," + 
+					"job_location varchar(64) not null," + 
+					"job_salary int," + 
+					"job_language varchar(32)," + 
+					"job_company varchar(64) not null," + 
+					"job_description text," + 
+					"job_residence varchar(32)," +
+					"job_employment varchar(16) not null" +
+					");"
+					;
+			
+			String sqlAdd3 = "insert into tb_job(job_title, job_location, job_salary, job_language, job_company, job_description, job_residence, job_employment) values ('Game Programmer', 'Tokyo, Japan', 4000000, 'JLPT N3', 'Rare LTD.', 'Game Engine Programmer for upcoming water based game on Xbox Series X and S consoles. Must be good with C++ and low-level languages', 'Japan Residents Only', 'Full-Time'), ('Clerk', 'Koriyama, Japan', 2500000, 'JLPT N2', 'Lawson', 'Assist in the stocking and checking out in our chain of convenience stores', 'Japan Residents Only', 'Part-Time'), ('Yoga Instructor', 'Iwaki, Japan', 3000000, 'JLPT N2', 'Prim Algo', 'Coach our clients to find inner peace and harmony', 'Will Sponsor Visa', 'Full-Time');";
+			
+			statement.executeUpdate(sqlStatement3);
+			statement.executeUpdate(sqlAdd1);
+			statement.executeUpdate(sqlAdd2);
+			statement.executeUpdate(sqlAdd3);
+
+
 		    
 		} catch(SQLException e) {
 			throw new RuntimeException(e);
